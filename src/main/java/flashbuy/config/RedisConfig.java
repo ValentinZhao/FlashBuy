d5f8f77b727f09897be64fcce34ed2df7e12b34a
ValentinZhao <2264709714@qq.com>
2020-02-22 18:42:04 -0800

    feat(master): import redis serializer config

--- src/main/java/flashbuy/config/RedisConfig.java
+++ src/main/java/flashbuy/config/RedisConfig.java
@@ -1,8 +1,17 @@
 package flashbuy.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import flashbuy.serializer.JodaDateTimeJsonDeserializer;
+import flashbuy.serializer.JodaDateTimeJsonSerializer;
 import org.apache.catalina.filters.CorsFilter;
+import org.joda.time.DateTime;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
@@ -11,4 +20,31 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Component
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 3600)
 public class RedisConfig {
+    @Bean
+    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory){
+        RedisTemplate redisTemplate = new RedisTemplate();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        //首先解决key的序列化方式
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+
+        //解决value的序列化方式
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+
+        ObjectMapper objectMapper =  new ObjectMapper();
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addSerializer(DateTime.class,new JodaDateTimeJsonSerializer());
+        simpleModule.addDeserializer(DateTime.class,new JodaDateTimeJsonDeserializer());
+
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+
+        objectMapper.registerModule(simpleModule);
+
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+
+        return redisTemplate;
+    }
 }
